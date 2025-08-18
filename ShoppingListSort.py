08c9e78c8fe55d5d5322af45da7063bc76653e9a
hahnworks <200227747+hahnworks@users.noreply.github.com>
2025-08-18 10:20:54 +0200

    Add Copyright notice

--- ShoppingListSort.py
+++ ShoppingListSort.py
@@ -1,3 +1,20 @@
+"""
+    This file is part of ShoppingListSort
+
+    Copyright (C) 2025 Alexander Hahn
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the European Union Public License (EUPL), version 1.2.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    European Union Public License for more details.
+
+    You should have received a copy of the European Union Public License
+    along with this program. If not, see <https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12>.
+"""
+
 import requests
 import re
 from openai import OpenAI
